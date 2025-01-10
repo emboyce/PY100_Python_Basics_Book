03413540cef513590dd03d80bae89695b3ebdb19
Eliza Boyce <elizaboyce@gmail.com>
2025-01-10 11:35:30 -0600

    Added Ch2 exercises

---
+++ Ch2_Data_Types/exercises.py
@@ -0,0 +1,13 @@
+names = ("Asta", "Butterscotch", "Pudding", "Neptune", "Darwin")
+print(type(names))
+
+pets = {
+	"Asta" : "dog",
+	"Butterscotch" : "cat",
+	"Pudding" : "cat",
+	"Neptune" : "fish",
+	"Darwin" : "lizard",
+	}
+
+print(type(pets))
+print(pets[names[0]]) 
