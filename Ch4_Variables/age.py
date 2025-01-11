e68f1208b7f2ac5b05462aa620ad54038c4c2747
Eliza Boyce <elizaboyce@gmail.com>
2025-01-10 21:56:17 -0600

    Added Ch4 exercises

---
+++ Ch4_Variables/age.py
@@ -0,0 +1,10 @@
+age = 42
+years = range(10, 50, 10)
+print(years)
+for i in years:
+    print(i)
+    print(f"In {i} years, you will be {age + i} years old")
+
+
+
+
---
+++ Ch4_Variables/greeter.py
@@ -0,0 +1,7 @@
+name = "Victor"
+print("Good morning", name)
+print("Yo", name)
+print("Salutations " + name)
+
+print(f"What's up {name}.")
+print(f"It's {name}'s birthday")
