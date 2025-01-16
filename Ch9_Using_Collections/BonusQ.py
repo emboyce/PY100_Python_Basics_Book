16ec769a3e320e09abb006872800dc2d0ae98fdf
Eliza Boyce <elizaboyce@gmail.com>
2025-01-16 00:10:36 -0600

    Added bonus exercise from Ch 9

---
+++ Ch9_Using_Collections/BonusQ.py
@@ -0,0 +1,12 @@
+# Use reversed() and join()
+reversed_string = "".join(reversed("Aloha, World!"))
+print("-".join(reversed("Aloha, World!")))
+# Output should be "!dlroW ,aholA"
+# print(reversed_string)
+
+numbers = []
+
+for i in range(1,6):
+    numbers.append(i)
+
+print(numbers)
